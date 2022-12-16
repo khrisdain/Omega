d6ca8a0461ea5ff3c6284381d066f2a460b89ee9
khrisdain <samuelilesanmi15@gmail.com>
2022-12-16 07:03:16 +0100

    futher changes

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,6 +1,18 @@
 import User from "../model/User.js";
 
 /* READ */
+export const getUser = async (req, res) => {
+    try{
+        const { id } = req.params;
+        const user = await User.findById(id);
+
+        res.status(200).json(user); 
+    }catch(err){
+        res.status(404).json({ message: err.message })
+    }
+}
+
+
 export const getUserFriends = async(req, res) => {
     try{
         const {id} = req.params; //Get users id from params
