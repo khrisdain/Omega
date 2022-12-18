61d4c17636ef0d94bfc052f397e0b27505ec6ba2
khrisdain <samuelilesanmi15@gmail.com>
2022-12-18 12:34:01 +0100

    initial commit

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -1,5 +1,34 @@
-const Home = () => {
-    return <div>Home Page</div>
-}
- 
-export default Home;
+import { createSlice } from  "@reduxjs/toolkit";
+
+const initialState = {
+    mode: "light", //toggle between dark and light
+    user: null,
+    token: null,
+    posts: []
+};
+
+export const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: { //actions( basically functions applied on global states)
+        setMode: (state) => {
+            state.mode = state.mode === "light" ? "dark" : "light"; 
+        },
+        setLogin: (state, action) => {
+            state.user = action.payload.user;
+            state.token = action.payload.token;
+
+        },
+        setLogout: (state) => {
+            state.user = null;
+            state.token = null;
+        },
+        setFriends: (state, action) => {
+            if(state.user) {
+                state.user.friends = action.payload.friends;
+            }else{
+                console.log("user friends non-existent")
+            }
+        }
+    }
+})
