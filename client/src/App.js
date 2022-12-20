40534fe019c791981e0eeff4799b257b6fa1bfd5
khrisdain <samuelilesanmi15@gmail.com>
2022-12-20 18:14:59 +0100

    some minor fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
-import HomePage from 'scenes/homePage'; 
-import LoginPage from 'loginPage';
+import HomePage from "scenes/homePage"; 
+import LoginPage from 'scenes/loginPage';
 import ProfilePage from 'scenes/profilePage';
 
 function App() {
