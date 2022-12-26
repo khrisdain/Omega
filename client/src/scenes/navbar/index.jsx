533580d934d1b99d5b4f08c134f9be139a254268
khrisdain <samuelilesanmi15@gmail.com>
2022-12-26 23:11:10 +0100

    minor changes

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -1,5 +1,39 @@
-const navbar = () => {
+import { useState } from "react";
+import {
+    box,
+    IconButton,
+    InputBase,
+    Typography,
+    Select,
+    MenuItem,
+    FormControl,
+    useTheme,
+    useMediaQuery
+} from "@mui/material";
+
+//icons
+import {
+    Search,
+    Message,
+    Darkmode,
+    LightMode,
+    Notification,
+    Help,
+    Menu,
+    Close
+} from "@mui/icons-material";
+
+import { useDispatch, useNavigate, useSelector } from "react-router-dom";
+import { setMode, setLogout } from "components/FlexBetween";
+
+const Navbar = () => {
+    const [ isMobileMenuToggled, setIsMobileMenuToggled ] = useState(false);  //small screen toggle
+    const dispatch = useDispatch() //dispatch actions fromm reducer
+    const Navigate = useNavigate();
+    const user = useSelector 
+
+
     return <div>Navbar</div>
 }
  
-export default navbar;
+export default Navbar;
