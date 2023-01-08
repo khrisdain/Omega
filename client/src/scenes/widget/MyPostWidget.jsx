5355cba27aab4efd3f61e08ab62d2ee0b7e82212
khrisdain <samuelilesanmi15@gmail.com>
2023-01-08 23:18:15 +0100

    Delete button implementation

--- client/src/scenes/widget/MyPostWidget.jsx
+++ client/src/scenes/widget/MyPostWidget.jsx
@@ -84,8 +84,40 @@ const MyPostwidget = ({ picturePath }) => {
               mt="1rem"
               p="1rem"
             >
-              <Dropzone>
-                
+              <Dropzone
+                acceptedFiles=".jpg,.jpeg,.png"
+                multiple={false}
+                onDrop={(acceptedFiles) => setImage(acceptedFiles[0])}
+            >
+              {({ getRootProps, getInputProps }) => (
+                <FlexBetween>
+                    <Box
+                    {...getRootProps()}
+                        border={`2px dashed ${palette.primary.main}`}
+                        p="1rem"
+                        width="100%"
+                        sx={{ "&:hover": { cursor: "pointer" } }}
+                    >
+                    <input {...getInputProps()} />
+                    {!image ? (
+                        <p>Add Image Here</p>
+                    ) : (
+                        <FlexBetween>
+                         <Typography>{image.name}</Typography>
+                         <EditOutlinedIcon />
+                        </FlexBetween>
+                    )}
+                    </Box>
+                    {image && (
+                      <IconButton
+                        onClick={() => setImage(null)}
+                        sx={{ width: "15%"}}
+                      >
+                        <DeleteOutlined />
+                      </IconButton>
+                    )}
+                </FlexBetween>
+              )}
               </Dropzone>
             </Box>
           )}
