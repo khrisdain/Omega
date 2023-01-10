c2c9cc18c233770193e648e64c64da8f0c3528f6
khrisdain <samuelilesanmi15@gmail.com>
2023-01-10 17:56:04 +0100

    fixed query issue

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -6,28 +6,24 @@ import Navbar from "../navbar/index";
 
 
 const HomePage = () => {
-    const isNonMobileScreens = useMediaQuery("(Min-width: 10000px)");
+    const isNonMobileScreens =useMediaQuery("(min-width: 1000px)")
     const { _id, picturePath } = useSelector((state) => state.user);
     return(
         <Box>
             <Navbar />
-            <Box
-              width="100%"
-              padding="2rem 6%"
-              display={ isNonMobileScreens ? "flex" : "block" }
-              gap="0.5rem"
-              justifyContent="space-between"
+            <Box 
+              width="100%" 
+              padding="2rem 6%" 
+              display={ isNonMobileScreens ? "flex" : "block" } 
+              gap="1rem"       
             >
               <Box flexBasis={ isNonMobileScreens ? "26%" : undefined}>
-                <UserWidget userId={_id} picturePath={picturePath} />
+                <UserWidget userId={ _id} picturePath={picturePath} />
               </Box>
-              <Box
-                flexBasis={isNonMobileScreens ? "42%" : undefined}
-                mt={ isNonMobileScreens ? undefined : "2rem"}
-              >
+
+              <Box flexBasis={isNonMobileScreens ? "42%  " : undefined} mt={ isNonMobileScreens ? undefined : "2rem"}>
                 <MyPostWidget picturePath={picturePath}  />
               </Box>
-              { isNonMobileScreens && <Box flexBasis="26%"></Box>}
             </Box>
         </Box>
     ) 
