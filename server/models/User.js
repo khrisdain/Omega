2ec44dc878da86c73bc1dbe4db8c79177e17daa8
khrisdain <samuelilesanmi15@gmail.com>
2023-01-11 17:05:33 +0100

    bug fixed

--- server/models/User.js
+++ server/models/User.js
@@ -25,7 +25,7 @@ const UserSchema = new mongoose.Schema(
             require: true,
             min: 5,
         },
-        picturPath: {
+        picturePath: {
             type: String,
             default: "",
         },
