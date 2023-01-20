8e2b9b3e6ab210d1b648077571d561e6661013e6
khrisdain <samuelilesanmi15@gmail.com>
2023-01-20 20:38:32 +0100

    Ad build up

--- client/src/scenes/widget/AdvertWidget.jsx
+++ client/src/scenes/widget/AdvertWidget.jsx
@@ -6,6 +6,19 @@ const AdvertWidget = () => {
     const { palette } = useTheme();
     const dark = palette.neutral.dark;
     const main = palette.neutral.main;
+    const medium = palette.neutral.medium;
+
+    return (
+        <WidgetWrapper>
+          <FlexBetween>
+            <Typography color={dark} variant="h5" fontWeight="500">
+              Sponsored 
+            </Typography>
+
+           <Typography color={medium}> Create ad. </Typography>
+          </FlexBetween>
+        </WidgetWrapper>
+    )
 
 }
 
