2468e0b4879773c128f2438d55ed03a348a1065a
khrisdain <samuelilesanmi15@gmail.com>
2023-01-20 20:34:32 +0100

    build up for advert widget

---
+++ client/src/scenes/widget/AdvertWidget.jsx
@@ -0,0 +1,12 @@
+import { Typography, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import WidgetWrapper from "components/WidgetWrapper";
+
+const AdvertWidget = () => {
+    const { palette } = useTheme();
+    const dark = palette.neutral.dark;
+    const main = palette.neutral.main;
+
+}
+
+export default AdvertWidget;
