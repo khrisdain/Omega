dee70b8db0e24b3c6f2a8232c171fd28558396b7
khrisdain <samuelilesanmi15@gmail.com>
2023-01-21 23:42:09 +0100

    Complete layout

--- client/src/scenes/widget/AdvertWidget.jsx
+++ client/src/scenes/widget/AdvertWidget.jsx
@@ -17,6 +17,22 @@ const AdvertWidget = () => {
 
            <Typography color={medium}> Create ad. </Typography>
           </FlexBetween>
+
+          <img 
+            widh="100%"
+            height="auto"
+            alt="advert"
+            src="http://localhost:3001/assets/info4.jpeg"
+            style={{ borderRadius: "0.75rem", margin: "0.75rem 0"}}
+          />
+          <FlexBetween>
+            <Typography color={main}>MikaCosmetics</Typography>
+            <Typography color={medium}>mikacosmetics.com</Typography>
+          </FlexBetween>
+
+          <Typography>
+           Your pathway to a stunning and immaculate beauty and made sure your skin is exfoliated and shinning like light 
+          </Typography>
         </WidgetWrapper>
     )
 
